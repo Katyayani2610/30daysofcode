a3744aa5f67d54e5493d5be9e03a7dc7603fb7de
Katyayani Agarwal <94683097+Katyayani2610@users.noreply.github.com>
2023-09-01 11:43:59 +0530

    Add files via upload

---
+++ conditionalstatement.cpp
@@ -0,0 +1,29 @@
+// C++ program to print alphabets
+#include <iostream>
+using namespace std;
+
+// Function to print the alphabet
+// in lower case
+void lowercaseAlphabets()
+{
+    // lowercase
+
+    for (char c = 'a'; c <= 'z'; ++c)
+        cout << c << " ";
+
+    cout << endl;
+}
+// Function to print the alphabet
+// in upper case
+void uppercaseAlphabets()
+{
+
+    // uppercase
+    for (char c = 'A'; c <= 'Z'; ++c)
+        cout << c << " ";
+
+    cout << endl;
+}
+
+
+
