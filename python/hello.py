7655b0e2a091a90a04c3a30dab4a7cf005eec02d
Katyayani Agarwal <94683097+Katyayani2610@users.noreply.github.com>
2025-07-08 21:47:54 +0530

    Add files via upload

---
+++ python/hello.py
@@ -0,0 +1,20 @@
+print("hello")
+
+def chai(n):
+    print(n)
+
+
+chai ("4")
+print("chai")
+
+
+print("2*3 = ", 2*3)
+print('"chai"*3 =', "chai" * 3)
+
+def chai(n):
+    print(n)
+chai("lemon tea ")
+chai_one="lemon tea"
+chai_two="gree tea"
+chai_three="masala tea"
+print(chai_one, chai_two, chai_three) 
---
+++ python/kat.py
@@ -0,0 +1,10 @@
+from hello import chai
+import os
+import sys 
+from importlib import reload
+print(os.getcwd())  # Prints the current working directory
+print("Hello, cutu!")
+print("hello cutu")
+
+for c in "chai":
+    print(c)
